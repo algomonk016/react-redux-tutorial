b54ac57c6eef5c1c7f60150bd94be5cc748083df
Shivesh Tiwari <algomonk016@gmail.com>
2021-12-10 00:03:16 +0530

    added redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
-import './App.css'
 import CakeComponents from './components/CakeComponents'
+import './App.css'
+
+import store from './redux/store'
+
 const App = () => {
   return (
-    <div className='App'>
-      <CakeComponents />
-    </div>
+    <Provider store={ store }>
+      <div className='App'>
+        <CakeComponents />
+      </div>
+    </Provider>
   )
 }
 
