655223b23b446513cb30438c4b6725c04310e69f
Shivesh Tiwari <algomonk016@gmail.com>
2021-12-12 20:30:13 +0530

    connect -> access state in components

--- src/components/CakeComponents.js
+++ src/components/CakeComponents.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
 
-function CakeComponents() {
+function CakeComponents(props) {
     return (
         <div>
-            <p> Number of cakes </p>
-            <button>Buy a Cake</button>
+            <p> Number of cakes {props.numOfCakes} </p>
+            <button onClick={props.buyCake}>Buy a Cake</button>
         </div>
     )
 }
 
-export default CakeComponents
+// when we need to access state in our component then we need to map our state
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeComponents)
